3b2aff48fbceb1e44e3fe10fae48f3f8ea676f2d
RajatNikam <86477706+RajatNikam@users.noreply.github.com>
2021-08-31 15:25:18 +0530

    Queue is Created

---
+++ Assignment Day 14 PP02/src/com/blz/Assignment14/Main.java
@@ -0,0 +1,13 @@
+package com.blz.Assignment14;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList myQueue = new LinkedList();
+        myQueue.enqueue(70);
+        myQueue.enqueue(30);
+        myQueue.enqueue(56);
+
+        myQueue.printList();
+    }
+}
